c49420156f21e029ad23f4fce44d8559d8436b95
Mauricio Oliveira <mau_oliver@hotmail.com>
2021-09-27 14:38:40 -0300

    Feat/ create cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Intervalo from './components/Intervalo'
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exercicio React-redux</h1>
+      <div className="linha">
+        <Intervalo></Intervalo>
+        <Card  title='Card 2'green>Y</Card>
+      </div>
+      <div className="linha">
+        <Card  title='Card 3'Blue>W</Card>
+        <Card  title='Card 4'Purple>Z</Card>
+      </div>
     </div>
   );
 }
